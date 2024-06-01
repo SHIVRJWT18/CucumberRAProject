36d57653ceeb5708d6008459c04ce41a7b9c9b24
SHIV SINGH <shivrajawat013489@outlook.com>
2024-06-01 20:58:30 +0530

    Commit on dated 010624

--- src/test/java/CucumberTestRunner/TestRunner.java
+++ src/test/java/CucumberTestRunner/TestRunner.java
@@ -3,7 +3,11 @@ package CucumberTestRunner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features= "F:\\SHIV NEWSCRIPTS\\CucumberRAProject\\src\\test\\java\\Features\\AddPlaceAPI.feature",glue = {"StepDefinition"})
+@CucumberOptions(
+features= "F:\\SHIV NEWSCRIPTS\\CucumberRAProject\\src\\test\\java\\Features\\AddPlaceAPI.feature"
+,glue = {"StepDefinition"}
+,dryRun = true)
+
 public class TestRunner extends AbstractTestNGCucumberTests
 {
 	//  ,tags = "@DeletePlace 
